6cf846cd4dd035413029f9154753d517511d8433
Linn Johansen <linn.es.johansen@gmail.com>
2025-03-19 14:47:53 -0700

    Bug fixes

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -8,8 +8,8 @@ import SpinnerMini from '../../ui/SpinnerMini';
 import FormRowVertical from '../../ui/FormRowVertical';
 
 function LoginForm() {
-  const [email, setEmail] = useState('linn@example.com');
-  const [password, setPassword] = useState('pass0926');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const { login, isLoading } = useLogin();
 
   function handleSubmit(e) {
